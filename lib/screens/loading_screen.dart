d0d14fb5decd76f38a445b10a21a49052c14bcef
UlrichMabou <ulrichmabou@gmail.com>
2020-02-09 21:19:21 +0300

    add method to fetch weather data

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -26,8 +27,21 @@ class _LoadingScreenState extends State<LoadingScreen> {
     print('Latitude: $lat , Longitude: $long');
   }
 
+  // function to get weather data for a location
+  void getData() async {
+    http.Response response = await http.get(
+        'https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22');
+    if (response.statusCode == 200) {
+      String data = response.body;
+      print(data);
+    } else {
+      print(response.statusCode);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    getData();
     return Scaffold();
   }
 }
