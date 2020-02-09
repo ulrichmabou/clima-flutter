108e6fe74ff306f61b27f730317ed7cdd42c1e35
UlrichMabou <ulrichmabou@gmail.com>
2020-02-09 22:52:04 +0300

    get live weather data and refactor

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
+import 'package:clima/services/networking.dart';
+
+const apiKey = '5ec3db72609ce34b2d6e538d4e19de2a';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -9,47 +10,34 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  double latitude;
+  double longitude;
+
   @override
   void initState() {
     super.initState();
 
-    getLocation();
+    getLocationData();
   }
 
   // function to get the device's location
-  void getLocation() async {
+  // and the weather for that location
+  void getLocationData() async {
     Location location = Location();
 
     await location.getCurrentLocation();
 
-    double lat = location.latitude;
-    double long = location.longitude;
+    latitude = location.latitude;
+    longitude = location.longitude;
 
-    print('Latitude: $lat , Longitude: $long');
-  }
+    NetworkHelper networkHelper = NetworkHelper(
+        'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
 
-  // function to get weather data for a location
-  void getData() async {
-    http.Response response = await http.get(
-        'https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22');
-    if (response.statusCode == 200) {
-      String data = response.body;
-
-      var decodedData = jsonDecode(data);
-
-      double temperature = decodedData['main']['temp'];
-      int condition = decodedData['weather'][0]['id'];
-      String cityName = decodedData['name'];
-
-      print('$cityName : $temperature / $condition');
-    } else {
-      print(response.statusCode);
-    }
+    var weatherData = await networkHelper.getData();
   }
 
   @override
   Widget build(BuildContext context) {
-    getData();
     return Scaffold();
   }
 }
